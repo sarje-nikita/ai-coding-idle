73f2db797a1b87d45115c3b7f2cabe6964d79c01
aniket.wagh <waniket48@gmail.com>
2026-05-17 21:27:09 +0530

    fix: update Vite server host configuration to use environment variable

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
@@ -11,12 +12,10 @@ export default defineConfig({
     },
   },
   server: {
-    host: '0.0.0.0',
+    host: process.env.VITE_HOST || 'localhost',
     port: 5173,
-    hmr: {
-      host: '15.206.153.229',
-      port: 5173,
-      protocol: 'http',
+    watch: {
+      usePolling: true,
     },
   },
 })
